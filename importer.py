90df4cd43c0377cae65c1325d2ed804dc640d6b1
KNProNooob <knpronoob@gmail.com>
2025-04-16 13:41:24 +0300

    Added fucntions that setup the datapack and the resourcepack

--- importer.py
+++ importer.py
@@ -2,10 +2,36 @@ import subprocess
 import os
 
 video_file_path = "input.mp4"
-resource_pack_path = "output"
-datapack_path = "functions"
+resource_pack_path = "resourcepack"
+datapack_path = "datapack"
 video_name = "test"
 namespace = "test"
+
+
+def CreateDatapack(datapack_path, namespace):
+    with open(datapack_path + "/pack.mcmeta", "w") as f:
+        f.write('{\n' \
+        '  "pack": {\n' \
+        '    "description": "MC Video Importer",\n' \
+        '    "pack_format": 18,\n' \
+        '  }\n' \
+        '}')
+    os.mkdir(datapack_path + "/data")
+    os.mkdir(datapack_path + "/data/" + namespace)
+    os.mkdir(datapack_path + "/data/" + namespace + "/function")
+
+def CreateResourcepack(resourcepack_path, namespace):
+    with open(resourcepack_path + "/pack.mcmeta", "w") as f:
+        f.write('{\n' \
+        '  "pack": {\n' \
+        '    "description": "MC Video Importer",\n' \
+        '    "pack_format": 46,\n' \
+        '  }\n' \
+        '}')
+    os.mkdir(resourcepack_path + "/assets")
+    os.mkdir(resourcepack_path + "/assets/" + namespace)
+    os.mkdir(resourcepack_path + "/assets/" + namespace + "/textures")
+    os.mkdir(resourcepack_path + "/assets/" + namespace + "/textures/overlays")
 
 def ConvertVideo(video_file_path, resource_pack_path):
     subprocess.run('ffmpeg -i ' + video_file_path + ' -vf "fps=20" ' + resource_pack_path + '/%d.png', shell=True)
@@ -24,6 +50,10 @@ def GenerateDatapack(resource_pack_path, datapack_path, video_name, namespace):
         f.writelines(frames)
         f.write("schedule function " + namespace + ":" + video_name + "/play 1t append")
 
-ConvertVideo(video_file_path, resource_pack_path)
 
-GenerateDatapack(resource_pack_path, datapack_path, video_name, namespace)
+CreateDatapack(datapack_path, namespace)
+CreateResourcepack(resource_pack_path, namespace)
+
+#ConvertVideo(video_file_path, resource_pack_path)
+
+#GenerateDatapack(resource_pack_path, datapack_path, video_name, namespace)
